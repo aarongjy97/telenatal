d9e5cba1343b323fbe5131123e2fbbd7b759aa18
Jing Ying <e0323887@u.nus.edu>
2021-07-10 00:08:36 +0800

    Configure theme

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-
+import './theme/vars.less';
 import Header from "./components/Header";
 import Overview from "./components/overview/Overview";
 
 export default function App() {
+  // document.body.classList.add('telenatal');
   return (
     <Router>
       <div id="root">
