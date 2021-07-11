ec4431c6825e2d6d98f59e384ba292da95598137
Jing Ying <e0323887@u.nus.edu>
2021-07-11 19:30:50 +0800

    Format code

--- src/components/records/RecordsMain.js
+++ src/components/records/RecordsMain.js
@@ -4,7 +4,7 @@ import Consultation from "./Consultation";
 import HealthRecord from "./HealthRecord";
 import Ultrasound from "./Ultrasound";
 import MedicalTest from "./MedicalTest";
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined } from "@ant-design/icons";
 
 const { TabPane } = Tabs;
 const { Content, Sider } = Layout;
@@ -16,7 +16,11 @@ export default function RecordsMain() {
   return (
     <Layout style={{ minHeight: "100vh" }}>
       {user === "DOCTOR" && (
-        <Sider className="site-layout-background" width={200} style={{paddingTop: "20px"}}>
+        <Sider
+          className="site-layout-background"
+          width={200}
+          style={{ paddingTop: "20px" }}
+        >
           <Menu
             mode="inline"
             defaultSelectedKeys={["1"]}
@@ -24,10 +28,18 @@ export default function RecordsMain() {
             style={{ height: "100%" }}
             theme="dark"
           >
-            <Menu.Item icon={<UserOutlined />} key="1">Patient 1</Menu.Item>
-            <Menu.Item icon={<UserOutlined />} key="2">Patient 2</Menu.Item>
-            <Menu.Item icon={<UserOutlined />} key="3">Patient 3</Menu.Item>
-            <Menu.Item icon={<UserOutlined />} key="4">Patient 4</Menu.Item>
+            <Menu.Item icon={<UserOutlined />} key="1">
+              Patient 1
+            </Menu.Item>
+            <Menu.Item icon={<UserOutlined />} key="2">
+              Patient 2
+            </Menu.Item>
+            <Menu.Item icon={<UserOutlined />} key="3">
+              Patient 3
+            </Menu.Item>
+            <Menu.Item icon={<UserOutlined />} key="4">
+              Patient 4
+            </Menu.Item>
           </Menu>
         </Sider>
       )}
@@ -43,22 +55,34 @@ export default function RecordsMain() {
           >
             <Consultation userType={user} />
           </TabPane>
-          <TabPane style={{
-            marginTop: "-16px",
-            paddingRight: "10px",
-          }} tab="Health Record" key="2">
+          <TabPane
+            style={{
+              marginTop: "-16px",
+              paddingRight: "10px",
+            }}
+            tab="Health Record"
+            key="2"
+          >
             <HealthRecord userType={user} />
           </TabPane>
-          <TabPane style={{
-            marginTop: "-16px",
-            paddingRight: "10px",
-          }} tab="Ultrasound Scan" key="3">
+          <TabPane
+            style={{
+              marginTop: "-16px",
+              paddingRight: "10px",
+            }}
+            tab="Ultrasound Scan"
+            key="3"
+          >
             <Ultrasound userType={user} />
           </TabPane>
-          <TabPane style={{
-            marginTop: "-16px",
-            paddingRight: "10px",
-          }} tab="Medical Test" key="4">
+          <TabPane
+            style={{
+              marginTop: "-16px",
+              paddingRight: "10px",
+            }}
+            tab="Medical Test"
+            key="4"
+          >
             <MedicalTest userType={user} />
           </TabPane>
         </Tabs>
