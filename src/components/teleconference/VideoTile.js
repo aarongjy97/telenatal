e5658deed8116159e5320ff240f8c8124c8fadae
Ashleigh <ashleighaathar@gmail.com>
2021-07-13 21:06:03 +0800

    mute self

--- src/components/teleconference/VideoTile.js
+++ src/components/teleconference/VideoTile.js
@@ -5,7 +5,13 @@ import { Spin } from "antd";
 export default function VideoTile(props) {
   return (
     <Spin spinning={props.loading}>
-      <video id="local-view" ref={props.localView} autoPlay playsInline></video>
+      <video
+        id="local-view"
+        ref={props.localView}
+        autoPlay
+        playsInline
+        muted
+      ></video>
       <video
         id="remote-view"
         ref={props.remoteView}
