91e602bcd545961f5ad33a4ebc04c202000916a0
Ashleigh <ashleighaathar@gmail.com>
2021-07-18 15:41:29 +0800

    make changes to components

--- src/components/meet/teleconference/AfterCallView.js
+++ src/components/meet/teleconference/AfterCallView.js
@@ -3,18 +3,6 @@ import { Row, Col, Card } from "antd";
 
 export default function AfterCallView(props) {
   return (
-<<<<<<< Updated upstream
-    <Row>
-      <Col>
-        <Card title="Appointment Ended" bordered={true}>
-          Thank you for attending your appointment Your doctor will be uploading
-          your appointment details soon, which you can view in the health
-          records tab.
-          {/* ASH TODO meeting deets */}
-        </Card>
-      </Col>
-    </Row>
-=======
     <>
       <Card title="Appointment Ended" bordered={true}>
         Thank you for attending your appointment Your doctor will be uploading
@@ -23,6 +11,5 @@ export default function AfterCallView(props) {
         {/* ASH TODO meeting deets */}
       </Card>
     </>
->>>>>>> Stashed changes
   );
 }
