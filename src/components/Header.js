f93970a19e25b5cec533a4972f518646da14b16c
gooohjy <gooohjiayi@gmail.com>
2021-07-18 17:55:32 +0800

    Show different header buttons based on login
    Using constant to toggle view for now before login is implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ import { Link, useLocation } from "react-router-dom";
 
 const { SubMenu } = Menu;
 
+const loggedIn = false;
+
 export default function Header() {
   const { Header } = Layout;
   let location = useLocation();
@@ -22,52 +24,54 @@ export default function Header() {
           <img src="logo.png" alt="TeleNatal Logo" height="40px" />
           TeleNatal
         </Col>
-        <Col className="buttons">
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={mappings[location.pathname]}
-          >
-            <Menu.Item key="1">
-              Overview
-              <Link to="/" />
-            </Menu.Item>
-            <Menu.Item key="2">
-              Meet
-              <Link to="/meet" />
-            </Menu.Item>
-            <Menu.Item key="3">
-              Records
-              <Link to="/records" />
-            </Menu.Item>
-            <Menu.Item key="4">
-              Appointments
-              <Link to="/appointments" />
-            </Menu.Item>
-            <SubMenu
-              key="SubMenu"
-              icon={
-                <Avatar
-                  size="medium"
-                  icon={
-                    <img
-                      src="avatar.jpg"
-                      alt="Profile"
-                      height="30px"
-                      style={{ borderRadius: "50%" }}
-                    />
-                  }
-                />
-              }
+        {loggedIn === true &&
+          <Col className="buttons">
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={mappings[location.pathname]}
             >
-              <Menu.Item key="5">
-                Profile
-                <Link to="/profile" />
+              <Menu.Item key="1">
+                Overview
+                <Link to="/overview" />
               </Menu.Item>
-              <Menu.Item key="">Logout</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Col>
+              <Menu.Item key="2">
+                Meet
+                <Link to="/meet" />
+              </Menu.Item>
+              <Menu.Item key="3">
+                Records
+                <Link to="/records" />
+              </Menu.Item>
+              <Menu.Item key="4">
+                Appointments
+                <Link to="/appointments" />
+              </Menu.Item>
+              <SubMenu
+                key="SubMenu"
+                icon={
+                  <Avatar
+                    size="medium"
+                    icon={
+                      <img
+                        src="avatar.jpg"
+                        alt="Profile"
+                        height="30px"
+                        style={{ borderRadius: "50%" }}
+                      />
+                    }
+                  />
+                }
+              >
+                <Menu.Item key="5">
+                  Profile
+                  <Link to="/profile" />
+                </Menu.Item>
+                <Menu.Item key="">Logout</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Col>
+        }
       </Row>
     </Header>
   );
