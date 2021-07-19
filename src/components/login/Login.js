ca606ddbe6d156a03ebaffbe9da30564a629ab22
gooohjy <gooohjiayi@gmail.com>
2021-07-20 01:30:26 +0800

    Brush up auth pages
    - Adding button to toggle between user registration
    - Styling changes (incl profile page)

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Layout, Upload, Button, message, Row, Col, Input, Form, Checkbox } from "antd";
-
-const user = "DOCTOR"; // Change this for now to toggle between modes
+import { Layout, Button, Row, Col, Input, Form, Switch } from "antd";
+import { LoginOutlined } from "@ant-design/icons";
 
 const formItemLayout = {
   labelCol: {
@@ -36,6 +35,7 @@ const tailFormItemLayout = {
 };
 
 export default function Login() {
+  const [showPatient, setShowPatient] = useState(true);
 
   const onFinish = (values) => {
     console.log('Success:', values);
@@ -49,52 +49,63 @@ export default function Login() {
     <Layout id="login">
       <Row className="row">
         <Col className="col" span={10}>
-          <p>Login</p>
-          <Form
-            {...formItemLayout}
-            name="loginForm"
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-          >
-            <Form.Item
-              label="E-mail"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your E-mail!',
-                },
-              ]}
+          <Row className="top">
+            <div className="title">
+              <LoginOutlined />&nbsp;Login {showPatient ? "Patient" : "Medical Professional"}
+            </div>
+            <div className="toggle">
+              <Switch
+                onClick={() => setShowPatient(!showPatient)}
+                defaultChecked />
+            </div>
+          </Row>
+          <Row className="bottom">
+            <Form
+              {...formItemLayout}
+              name="loginForm"
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
             >
-              <Input />
-            </Form.Item>
+              <Form.Item
+                label="E-mail"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your E-mail!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your password!',
-                },
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
 
-            <Form.Item {...tailFormItemLayout}>
-              <Button type="primary" htmlType="submit">
-                Login
-              </Button>
-              &nbsp;&nbsp;&nbsp;
-              <Button type="default" href="/">
-                Back to Home
-              </Button>
-              <Button type="link" href="/register">
-                New here? Register
-              </Button>
-            </Form.Item>
-          </Form>
+              <Form.Item {...tailFormItemLayout}>
+                <Button type="primary" htmlType="submit">
+                  Login
+                </Button>
+                &nbsp;&nbsp;&nbsp;
+                <Button type="default" href="/">
+                  Back to Home
+                </Button>
+                <Button type="link" href="/register">
+                  New here? Register
+                </Button>
+              </Form.Item>
+            </Form>
+          </Row>
         </Col>
       </Row>
     </Layout>
