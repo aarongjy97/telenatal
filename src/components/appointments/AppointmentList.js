1ebb79b4aab186f5c30d54f62d1d2965566527bc
gooohjy <gooohjiayi@gmail.com>
2021-07-30 14:18:30 +0800

    Show message if no appointment

--- src/components/appointments/AppointmentList.js
+++ src/components/appointments/AppointmentList.js
@@ -1,26 +1,39 @@
-import React from "react";
-import { Layout, Row, List } from "antd";
+import React, { useState } from "react";
+import { Layout, Row, List, Spin } from "antd";
 import AppointmentCard from "./AppointmentCard";
+import { isDictEmpty } from "./../utils";
 
 export default function AppointmentList({ upcomingAppointments }) {
+  const [showList, setShowList] = useState(false);
+
+  setTimeout(function () {
+    setShowList(true);
+  }, 1000);
+
   return (
     <Layout id="appointmentList">
       <Row className="title">Upcoming Appointments</Row>
+      <Row className="spin">
+        {!showList && <Spin />}
+        {showList && isDictEmpty(upcomingAppointments) && (
+          <p>Please book an appointment!</p>
+        )}
+      </Row>
       <Row className="content">
-        <List
-          grid={{ gutter: 16, column: 1 }}
-          dataSource={upcomingAppointments}
-          itemLayout="vertical"
-          renderItem={(appointment) => {
-            return (
-              <List.Item key="appointment.appointmentId">
-                <AppointmentCard
-                  appointment={appointment}
-                />
-              </List.Item>
-            );
-          }}
-        />
+        {showList && !isDictEmpty(upcomingAppointments) && (
+          <List
+            grid={{ gutter: 16, column: 1 }}
+            dataSource={upcomingAppointments}
+            itemLayout="vertical"
+            renderItem={(appointment) => {
+              return (
+                <List.Item key="appointment.appointmentId">
+                  <AppointmentCard appointment={appointment} />
+                </List.Item>
+              );
+            }}
+          />
+        )}
       </Row>
     </Layout>
   );
--- src/components/meet/appointments/AppointmentList.js
+++ src/components/meet/appointments/AppointmentList.js
@@ -1,28 +1,43 @@
-import React from "react";
-import { Layout, Row, List } from "antd";
+import React, { useState } from "react";
+import { Layout, Row, List, Spin } from "antd";
 import AppointmentCard from "./AppointmentCard";
+import { isDictEmpty } from "./../../utils";
 
 export default function AppointmentsList({
   upcomingAppointments,
   onAppointmentTileClick,
 }) {
+  const [showList, setShowList] = useState(false);
+
+  setTimeout(function () {
+    setShowList(true);
+  }, 1000);
+
   return (
     <Layout id="appointmentListMeet">
       <Row className="title">Upcoming Appointments</Row>
+      <Row className="spin">
+        {!showList && <Spin />}
+        {showList && isDictEmpty(upcomingAppointments) && (
+          <p>Please book an appointment via the 'Appointments' Tab!</p>
+        )}
+      </Row>
       <Row className="content">
-        <List
-          grid={{ gutter: 16, column: 1 }}
-          dataSource={upcomingAppointments}
-          itemLayout="vertical"
-          renderItem={(appointment) => (
-            <List.Item
-              key={appointment.appointmentId}
-              onClick={() => onAppointmentTileClick(appointment)}
-            >
-              <AppointmentCard appointment={appointment} />
-            </List.Item>
-          )}
-        />
+        {showList && !isDictEmpty(upcomingAppointments) && (
+          <List
+            grid={{ gutter: 16, column: 1 }}
+            dataSource={upcomingAppointments}
+            itemLayout="vertical"
+            renderItem={(appointment) => (
+              <List.Item
+                key={appointment.appointmentId}
+                onClick={() => onAppointmentTileClick(appointment)}
+              >
+                <AppointmentCard appointment={appointment} />
+              </List.Item>
+            )}
+          />
+        )}
       </Row>
     </Layout>
   );
